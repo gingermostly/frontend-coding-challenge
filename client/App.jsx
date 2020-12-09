30f6bb4ff747af9dc9f6fb38b58f8844ba5f44b3
gingermostly <gingermostly@gmail.com>
2020-12-09 11:45:15 -0500

    add css grid styling, font styling

--- client/App.jsx
+++ client/App.jsx
@@ -17,7 +17,6 @@ class App extends React.Component {
                     data: res.data.items
             })
         })
-        console.log(this.state.data)
     }
     render () {
         return (
@@ -27,14 +26,16 @@ class App extends React.Component {
             </button>
             {this.state.data.map(item => {
                 return (
-                <div>
+                <div className='container'>
                     <img src={item.owner.avatar_url}/>
-                    <div>GitHub Username: {item.owner.login}</div>
-                    <div>repo name: {item.name}</div>
-                    <div>description: {item.description}</div>
+                    <div className='repo-wrapper'>
+                        <div className='title'>{item.name}</div>
+                        <div className='description'>{item.description}</div>
+                    </div>
                     <div># of stars: {item.stargazers_count}</div>
                     {/* should issues include all issues or just current open issues? */}
                     <div># of issues: {item.open_issues}</div>
+                    <div>GitHub Username: {item.owner.login}</div>
                 </div>
                 )
             })}

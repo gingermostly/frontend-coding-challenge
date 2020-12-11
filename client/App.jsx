644dfacabba113975fc40de0136b730d5e69c6ba
gingermostly <gingermostly@gmail.com>
2020-12-11 16:34:14 -0500

    Add scroll behavior to div to allow checking when scrolling has reached bottom of page

--- client/App.jsx
+++ client/App.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import './styles.css';
 import axios from 'axios';
 
@@ -8,6 +8,8 @@ const pastThirty = new Date().setDate(getCurrent.getDate() - 30);
 
 const searchDate = new Date(pastThirty).toISOString().slice(0, 10);
 
+const currentPage = 1;
+
 class App extends React.Component {
     constructor(props){
         super(props);
@@ -15,23 +17,31 @@ class App extends React.Component {
             data: []
         }
         this.handleClick = this.handleClick.bind(this);
+        this.handleScroll = this.handleScroll.bind(this);
     }
     handleClick() {
-        axios.get(`https://api.github.com/search/repositories?q=created:>${searchDate}&sort=stars&order=desc&page=1&per_page=50`)
+        axios.get(`https://api.github.com/search/repositories?q=created:>${searchDate}&sort=stars&order=desc&page=${currentPage}&per_page=50`)
             .then(res => {
                 this.setState({
                     data: res.data.items
             })
         })
     }
+    handleScroll(e) {
+        let height = e.target.scrollHeight;
+        let top = e.target.scrollTop;
+        let client = e.target.clientHeight;
+        if (height - top === client) {
+            console.log('end of page!')
+        }
+    }
     render () {
         return (
-        <div>
+        <div className='scroll-container' onScroll={this.handleScroll}>
             <button onClick={this.handleClick}>
                 GET REPOS
             </button>
             {this.state.data.map(item => {
-                console.log(this.state.data)
                 return (
                 <div className='container'>
                     <img src={item.owner.avatar_url}/>
